9536b867be36a4cd2dc0fabbb3544c693be2d0df
hnifmaghfur <hnifmaghfur@gmail.com>
2020-03-27 11:32:00 +0700

    Menambahkan library Material / sejenis boostrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, MatExpansionModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostOutputComponent } from './posts/post-output/post-output.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostOutputComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-output/post-output.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-post-output',
+  templateUrl: './post-output.component.html',
+  styleUrls: ['./post-output.component.css']
+})
+
+export class PostOutputComponent {
+
+}
