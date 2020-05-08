0a15871afcf308bc7fa30e822265db4290456595
hnifmaghfur <hnifmaghfur@gmail.com>
2020-05-08 14:03:26 +0700

    Penambahan input pada database MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const Post = require('./model/post');
 const app = express();
+
+mongoose.connect('mongodb+srv://hanif:Nifinda7@dbangular-oj9z7.mongodb.net/database?retryWrites=true&w=majority', { useNewUrlParser: true })
+.then(() => {
+  console.log('Connected to Database!');
+})
+.catch(() => {
+  console.log('Connection Database Failed!');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,8 +30,12 @@ app.use((req, res, next) => {                           //untuk memberikan akses
 });
 
 app.post('/api/posts',(req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log( post );
+  post.save();
   res.status(201).json({
     messange: 'Post added successfully'
   });
@@ -38,6 +52,7 @@ app.use('/api/posts',(req, res, next) => {
       content: 'Ini adalah Post Kedua'
     }
   ];
+
   return res.status(200).json({
     messange: 'Berhasil input Post',
     posts: posts
